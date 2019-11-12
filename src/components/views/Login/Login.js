1f13f1be96807d80a0202a15bccc6310469fb37d
Maciej Kryczka <maciejkryczka@hotmail.com>
2019-11-12 11:10:51 +0100

    add login page

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import styles from './Login.module.scss';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Paper from '@material-ui/core/Paper';
 
 const Login = () => {
   return(
-    <div className={styles.component}>
-      <h2>LOGIN VIEW</h2>
-    </div>
+    <Paper className={styles.component}>
+      <div>
+        <TextField
+          label="Login"
+          placeholder="enter your login"
+          margin="dense"
+          fullWidth
+        />
+      </div>
+      <div>
+        <TextField
+          label="Pasword"
+          placeholder="enter your pasword"
+          margin="dense"
+          fullWidth
+        />
+      </div >
+      <div>
+        <Button className={styles.button} variant="contained" color="primary">
+          login
+        </Button>
+      </div>
+    </Paper>
   );
 };
 
